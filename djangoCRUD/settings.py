2271b1db288bc4f27e915dac6ad26c64661c5cef
zaysevkun <zaysev100@gmail.com>
2020-09-23 00:54:03 +1000

    postgres connected and secret keys/passwords hidden in .env

--- djangoCRUD/settings.py
+++ djangoCRUD/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')8y$g$3xgfx%22=jj@mwji=&!!)s+n!$&3^=ved44o#v_a_)8_'
-
+test = os.path.expanduser('~/PycharmProjects/djangoCRUD')
+test = os.path.join(test, '.env')
+load_dotenv(test)
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'djangoCRUD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
